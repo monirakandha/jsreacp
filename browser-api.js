efd49fa1c3d26816fa74215196b67b9736b7c3fb
monirhosenakanda <monirhosenakanda@gmail.com>
2022-03-08 23:30:37 +0600

    browser api start

---
+++ browser-api.js
@@ -0,0 +1,3 @@
+// ব্রাউজার API সম্পর্কে চারটা জিনিস 
+// ১. লোকাল স্টোরেজ, সেশন স্টোরেজ কোনটা কখন ইউজ করবে। কিভাবে ইউজ করবে 
+// sessionStorage and localStorage are same but it have different while data in localStorage doest not exprise and when data in sessionStorage is cleared when the page and browser also tab is closed. 
