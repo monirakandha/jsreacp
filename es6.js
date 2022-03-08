ca772ae5ebfea27effabaa8d59f6236ce44b4edd
monirhosenakanda <monirhosenakanda@gmail.com>
2022-03-08 22:21:55 +0600

    3.4 problem sloved

--- es6.js
+++ es6.js
@@ -1,27 +1,44 @@
-const numbers = [89, 35, 98, 12];
-const student = {
-    name: 'Salib Khan',
-    age: 32,
-    movies: ['king khan', 'Dhakar Mastan']
-};
+// const numbers = [89, 35, 98, 12];
+// const student = {
+//     name: 'Salib Khan',
+//     age: 32,
+//     movies: ['king khan', 'Dhakar Mastan']
+// };
 
-console.log(`My Name is ${student.name} amd my age is ${student.age} our movies is ${student.movies} my favourite movie is ${student.movies[0]}`)
+// console.log(`My Name is ${student.name} amd my age is ${student.age} our movies is ${student.movies} my favourite movie is ${student.movies[0]}`)
 
 // ২. স্প্রেড অপারেটর (...) কিভাবে কাজ করে। বিশেষ করে একটা array কে কপি করে নতুন করে array বানাবে এবং সেখানে একটা উপাদান যোগ করবে। আবার একটা উপাদান কে বাদ দিয়ে বাকি সব উপাদানকে কিভাবে যোগ করবে (filter ইউজ করে)
 
 // const newNumber = [...numbers , 90,890,098];
 // console.log(newNumber);
 
-const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
-const result = words.filter(word => word > 6);
-console.log(result);
+// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+// const result = words.filter(word => word > 6);
+// console.log(result);
 
 // ৩.১. শূন্য প্যারামিটারওয়ালা একটা অ্যারো ফাংশন লিখবে যেটা ৯ রিটার্ন করবে।
- const firstFun = () => 9;
- console.log(firstFun());
+//  const firstFun = () => 9;
+//  console.log(firstFun());
 
 
 //  ৩.২. এক প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন ডিক্লেয়ার করবে। এই ফাংশনের কাজ হবে যে প্যারামিটার নিবে সেটাকে ১২ দিয়ে গুণ করে গুণফল রিটার্ন করবে। 
 
-const secindFun = (value) => value * 12;
-console.log(secindFun(12));
+// const secindFun = (value) => value * 12;
+// console.log(secindFun(12));
+
+// ৩.৩ দুই, প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন লিখবে। এই ফাংশনের কাজ হবে যে দুইটা প্যারামিটার ইনপুট নিবে। সেই দুইটা প্যারামিটারকে যোগ করে যোগফলকে চার দিয়ে ভাগ করে ভাগফল রিটার্ণ করে দাও। 
+
+// const thirdFun = (num1,num2) => (num1 + num2)/4;
+// console.log(thirdFun(30, 40));
+
+
+// ৩.৪ একাধিক লাইনওয়ালা অ্যারো ফাংশন লিখো। সেটাতে দুইটা প্যারামিটার নিবে। ওই arrow ফাংশনটা হবে অনেকগুলা লাইনের। সেখানে প্রত্যেকটা ইনপুট প্যারামিটার এর সাথে ৫ যোগ করবে তারপর যোগফল দুইটাকে আবার গুণ করবে। ক্যামনে করবে সেটা চিন্তা করে বের করার চেষ্টা করো। 
+
+const fourthFun =(num1 , num2)=> {
+    const resultOne = num1 + 5;
+    const resultTwo = num2 + 5;
+    const newResult = resultOne * resultTwo;
+    return newResult;
+}
+
+console.log(fourthFun(598,98765644));
