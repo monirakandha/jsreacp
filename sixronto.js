a02c71d3292a1679387fb84ad2529294633ee972
monirhosenakanda <monirhosenakanda@gmail.com>
2022-03-08 10:09:51 +0600

    first problem solve

---
+++ sixronto.js
@@ -0,0 +1,8 @@
+// প্রোগ্রামিংয়ের ছয়রত্ন কি তুমি কি জানো: 
+
+// ১. কিভাবে let, const দিয়ে ভেরিয়েবল লিখতে হয়,কখন কোনটা ইউজ করে তুমি কি জানো ?
+
+let number = 45;
+let job = 'support engineer';
+const name = 'monir hosen akandha';
+console.log(number,job,name);
