c9768451383bc878314aa9368e8dbebae0a881ea
monirhosenakanda <monirhosenakanda@gmail.com>
2022-03-08 10:59:16 +0600

    problem 2.1 solved

--- sixronto.js
+++ sixronto.js
@@ -5,4 +5,81 @@
 let number = 45;
 let job = 'support engineer';
 const name = 'monir hosen akandha';
-console.log(number,job,name);
+// console.log(number,job,name);
+
+// ২.১ কিভাবে কন্ডিশন লিখতে হয়, ছয় রকমের কন্ডিশন (>, <, ===, !==, >=. <=) কোনটা কোন জিনিসের জন্য ইউজ করবে। এছাড়াও && বা ।। দিয়ে কিভাবে একাধিক কন্ডিশন এর মধ্যে দুইটাই ফুলফিল করতে হবে আবার দুইটার যেকোন একটা ফুলফিল করতে হবে, সেটা কিভাবে করবে 
+
+// let num1 = 23;
+// let num2 = 45;
+
+// if(num1 > num2){
+//     comsole.log('num1 is small');
+// }
+// else{
+//     console.log('num2 is small')
+// }
+
+// let num1 = 56;
+// let num2 = 78;
+// if(num1 <  num2){
+//     console.log('num1 is small');
+
+// }
+// else{
+//     console.log('num2 is small');
+// }
+
+// let num1 = 89;
+// let num2 = 89;
+// if(num1 === num2){
+//     console.log('num1 and num2 are same data type');
+// }
+// else{
+//     console.log('num1 and num2 are not same but data type same');
+// }
+
+// let num1 = 89;
+// let num2 = 89;
+// if(num1 !== num2){
+//     console.log('num1 and num2 are same data type');
+// }
+// else{
+//     console.log('num1 and num2 are not same but data type same');
+// }
+
+
+// let num1 = 89;
+// let num2 = 89;
+// if(num1 >= num2){
+//     console.log('num1 and num2 are same data type');
+// }
+// else{
+//     console.log('num1 and num2 are not same but data type same');
+// }
+
+
+// let num1 = 89;
+// let num2 = 89;
+// if(num1 <= num2){
+//     console.log('num1 and num2 are same data type');
+// }
+// else{
+//     console.log('num1 and num2 are not same but data type same');
+// }
+
+const fatherName = 'Arnold';
+let season = 'rainy';
+
+// if(fatherName === 'android' || season === 'rainy'){
+//     console.log('ok');
+// }
+// else{
+//     console.log('check again');
+// }
+
+// if(fatherName === 'android' && season === 'rainy'){
+//     console.log('ok');
+// }
+// else{
+//     console.log('check again');
+// }
